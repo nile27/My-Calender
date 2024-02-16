ce9c87477239eb4ef3fb0618b1ae9882ab59bb17
nile27 <ddj03104@gmail.com>
2024-02-16 11:01:53 +0900

    Month datePicker 컴포넌트 완료

--- src/Components/Main_AddTodo.tsx
+++ src/Components/Main_AddTodo.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import styled from "styled-components";
-import ReactDatePicker, { CalendarContainer } from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { ko } from "date-fns/locale";
 
 import Xbtn from "../Img/ph_x-bold.svg";
-import Arrow from "../Img/ep_arrow-right-bold.svg";
+import Clock from "../Img/tabler_clock.svg";
+import DatePicker from "./DatePicker";
 
 // import axios from "axios";
 
@@ -73,22 +71,29 @@ const ModalInput = styled.input`
 
 const ModalTime = styled.div`
   width: 100%;
-  height: 100%;
+  height: auto;
   border-bottom: 1px solid var(--light-gray);
-  padding: 10px;
-  background: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  background: var(--line-gray);
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
 `;
 
-const TimeBlock = styled.div`
+const DateBlock = styled.div`
   width: 100%;
   height: auto;
-  background: var(--line-gray);
+  background-color: none;
   display: flex;
   justify-content: start;
   align-items: center;
   flex-direction: column;
 
-  box-shadow: 5px 5px 5px 5px var(--line-gray);
   padding: 1rem;
   position: relative;
 
@@ -98,120 +103,6 @@ const TimeBlock = styled.div`
     justify-content: space-between;
     align-items: center;
   }
-`;
-const DateWapper = styled.div`
-  position: relative;
-  z-index: 200;
-  .react-datepicker {
-    padding: 16px;
-    width: 300px;
-
-    .react-datepicker__header {
-      background-color: #fff;
-      color: #fff;
-      border-bottom: none;
-      border-radius: 0;
-    }
-
-    .react-datepicker__month-container {
-      padding-bottom: 16px;
-      margin-bottom: 8px;
-      border-bottom: 1px solid #d4d6dd;
-
-      .react-datepicker__day-names {
-        width: 280px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-sizing: border-box;
-
-        .react-datepicker__day-name {
-          display: flex;
-          width: 40px;
-          height: 40px;
-          justify-content: center;
-          align-items: center;
-        }
-      }
-
-      .react-datepicker__current-month {
-        float: left;
-      }
-
-      .react-datepicker__month {
-        margin: 0px;
-      }
-
-      .react-datepicker__week {
-        width: 280px;
-        display: flex;
-        justify-content: space-around;
-
-        > * {
-          display: flex;
-          width: 40px;
-          height: 40px;
-          justify-content: center;
-          align-items: center;
-          color: var(--neutral-dark-medium, #494a50);
-          text-align: center;
-
-          font-family: Inter;
-          font-size: 12px;
-          font-style: normal;
-          font-weight: 700;
-          line-height: normal;
-        }
-
-        .react-datepicker__day--selected {
-          border-radius: 20px;
-          background: var(--highlight-darkest, #006ffd);
-          display: flex;
-          width: 40px;
-          height: 40px;
-          justify-content: center;
-          align-items: center;
-          color: #fff;
-        }
-      }
-    }
-
-    .react-datepicker__children-container {
-      width: 300px;
-    }
-  }
-`;
-const StyledDatePicker = styled(ReactDatePicker)`
-  .react-datepicker {
-    padding: 16px;
-    width: 300px;
-
-    .react-datepicker__header {
-      background-color: #fff;
-      color: #fff;
-      border-bottom: none;
-      border-radius: 0;
-    }
-  }
-`;
-
-const Customhead = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 1rem;
-  padding: 0 1rem;
-  color: black;
-  span {
-    font-weight: 500;
-  }
-  .btnBox {
-    display: flex;
-    gap: 1rem;
-  }
-  margin: 5px 0;
 `;
 
 const DateBtn = styled.button`
@@ -231,20 +122,6 @@ const DateBtn = styled.button`
   }
 `;
 
-const PrevButton = styled.div`
-  style: none;
-  cursor: pointer;
-  width: 15px;
-  height: 15px;
-  background-image: url(${Arrow});
-  background-size: cover;
-`;
-
-// 다음 달로 이동하는 버튼
-const NextButton = styled(PrevButton)`
-  transform: scaleX(-1);
-`;
-
 interface Prop {
   modal: boolean;
   setModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -258,16 +135,6 @@ export default function AddTodo(props: Prop) {
 
   // const timeArr = Array.from({ length: 24 }, (_, i) => i + 1);
 
-  const handleDatePicker = (date: [Date, Date]) => {
-    if (date[1]) {
-      setDatePicker(!datepicker);
-    }
-    setStartDate(date[0]);
-    setEndDate(date[1]);
-
-    console.log(startDate, endDate);
-  };
-
   return (
     <>
       <BackgroundBox onClick={() => setModal(!modal)}></BackgroundBox>
@@ -280,7 +147,8 @@ export default function AddTodo(props: Prop) {
         <ModalInputBox>
           <ModalInput placeholder="제목" />
           <ModalTime>
-            <TimeBlock>
+            <img src={Clock}></img>
+            <DateBlock>
               <div className="flexBtn">
                 <DateBtn onClick={() => setDatePicker(true)}>
                   <span>
@@ -294,35 +162,16 @@ export default function AddTodo(props: Prop) {
                 </DateBtn>
               </div>
               {datepicker ? (
-                <DateWapper>
-                  <StyledDatePicker
-                    locale={ko}
-                    startDate={startDate}
-                    endDate={endDate}
-                    onChange={(date: [Date, Date]) => handleDatePicker(date)}
-                    selectsRange
-                    closeOnScroll={true}
-                    inline
-                    popperContainer={CalendarContainer}
-                    renderCustomHeader={({
-                      date,
-                      decreaseMonth,
-                      increaseMonth,
-                    }) => (
-                      <Customhead>
-                        <span>
-                          {`${date.getFullYear()}년 ${date.getMonth() + 1}월`}
-                        </span>
-                        <div className="btnBox">
-                          <PrevButton onClick={decreaseMonth}></PrevButton>
-                          <NextButton onClick={increaseMonth}></NextButton>
-                        </div>
-                      </Customhead>
-                    )}
-                  />
-                </DateWapper>
+                <DatePicker
+                  startDate={startDate}
+                  endDate={endDate}
+                  datepicker={datepicker}
+                  setDatePicker={setDatePicker}
+                  setStartDate={setStartDate}
+                  setEndDate={setEndDate}
+                />
               ) : null}
-            </TimeBlock>
+            </DateBlock>
           </ModalTime>
         </ModalInputBox>
       </Container>
